0c3361218f4f0e487a07af44c92b28fc4b91b161
MinJeong <Andante.mjkim@gmail.com>
2025-05-19 23:30:42 +0900

    Create 2501.py

---
+++ basic_BaekJoon/2501.py
@@ -0,0 +1,14 @@
+# 백준 2501 : 약수 구하기
+
+N, K = map(int, input().split())
+
+divisor = []
+
+for i in range(1, N+1):
+    if N%i == 0:
+        divisor.append(i)
+
+if len(divisor) < K:
+    print(0)
+else:
+    print(divisor[K-1])
